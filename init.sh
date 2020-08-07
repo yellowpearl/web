82c1471e18b951ece655945bbcd304a0a3e14392
Yellowpearl <Yellowpearl@yandex.ru>
2020-08-07 21:04:19 +0300

    2

---
+++ init.sh
@@ -0,0 +1,7 @@
+chmod a+x init.sh
+cp /etc/nginx/nginx.conf ~/web/etc
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
