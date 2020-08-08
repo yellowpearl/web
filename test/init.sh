7f8fdbe1ef2717b4b1fd6d6a43a7778e407d9a50
yellowpearl <yellowprearl@yandex.ru>
2020-08-08 19:34:29 +0300

    nakinec rabochiy variant

--- test/init.sh
+++
@@ -1,7 +0,0 @@
-chmod a+x init.sh
-cp /etc/nginx/nginx.conf ~/web/etc
-sudo ln -sf /home/box/web/etc/nginx.conf  /home/yellowpearl/PycharmProjects/untitled1/test
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
---
+++ test/pull_cp.sh
@@ -0,0 +1,3 @@
+sudo ln -sf /etc/nginx/nginx.conf  ./
+sudo cp ./config.conf ./nginx.conf
+sudo /etc/init.d/nginx restart
