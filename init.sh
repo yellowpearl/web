516b4a3eda87288e3c6504fddd62d4c864dcccbf
yellowpearl <yellowprearl@yandex.ru>
2020-08-09 14:12:41 +0300

    1steps gunicorn

--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+gunicorn -c hello.py hello:wsgi_application
