f39394a8709079d8588d7791c561c4c308bc2263
yellowpearl <yellowprearl@yandex.ru>
2020-08-09 14:25:49 +0300

    rabotaet bez /n

--- hello/hello.py
+++ hello/hello.py
@@ -2,17 +2,11 @@
 bind="0.0.0.0:8080"
 
 
-def query_func(lst):
-    query = lst['QUERY_STRING']
-    body = [bytes(i + '\n', 'ascii') for i in query.split('&')]
-    return body
-
-
 def wsgi_application(environ, start_response):
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    body = query_func(environ)
     start_response(status, headers)
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     return body
