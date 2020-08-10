25a53ec213cc3b34b65ec83a6e5c7a2e0dc47ba3
yellowpearl <yellowprearl@yandex.ru>
2020-08-10 16:43:56 +0300

    up up

--- ask/up.sh
+++ ask/up.sh
@@ -6,3 +6,8 @@ sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade django==2.1
 sudo pip3 install --upgrade gunicorn
+virtualenv -p python3 myvenv
+source myvenv/bin/activate
+pip install --upgrade pip
+pip install django
+pip install gunicorn
